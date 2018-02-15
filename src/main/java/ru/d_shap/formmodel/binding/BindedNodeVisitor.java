a020db6d4ba54243fa9f4c893f0e9e6e98c89e08
dmitry@DMITRY-DELL.Dlink <dmitry@DMITRY-DELL.Dlink>
2018-02-15 23:00:22 +0300

    Интерфейс для реализации шаблона Visitor

---
+++ src/main/java/ru/d_shap/formmodel/binding/BindedNodeVisitor.java
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding;
+
+/**
+ * Binded node visitor.
+ *
+ * @author Dmitry Shapovalov
+ */
+public interface BindedNodeVisitor {
+
+    /**
+     * Visit the binded node.
+     *
+     * @param bindedNode the binded node.
+     */
+    void visit(BindedNode bindedNode);
+
+}
