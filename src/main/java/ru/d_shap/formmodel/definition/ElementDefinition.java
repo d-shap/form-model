08f55fbcfac92da925b46d47ed35c645165cb8db
dmitry@DMITRY-DELL.Dlink <dmitry@DMITRY-DELL.Dlink>
2018-02-16 14:08:00 +0300

    Исправил ошибки в заголовке и javadoc

--- src/main/java/ru/d_shap/formmodel/definition/ElementDefinition.java
+++ src/main/java/ru/d_shap/formmodel/definition/ElementDefinition.java
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-// Form model library is form definition API and form binding API.
+// Form model library is a form definition API and a form binding API.
 // Copyright (C) 2018 Dmitry Shapovalov.
 //
 // This file is part of form model library.
@@ -14,7 +14,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
-// You should have received copy of the GNU Lesser General Public License
+// You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.formmodel.definition;
@@ -38,7 +38,6 @@ import java.util.Set;
  * <p>
  * Binded element can be used to access properties (for example, the text edit field value) or to perform
  * actions (for example, to press the button).
- * <p>
  * </p>
  *
  * @author Dmitry Shapovalov
