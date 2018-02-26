fd9cf3942c3b99ffc2aaac03bbef3ae5961adc44
dmitry@DMITRY-DELL.Dlink <dmitry@DMITRY-DELL.Dlink>
2018-02-26 23:13:57 +0300

    Поправил javadoc

--- src/main/java/ru/d_shap/formmodel/binding/package-info.java
+++ src/main/java/ru/d_shap/formmodel/binding/package-info.java
@@ -18,6 +18,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
+ * <p>
  * Form binding API.
+ * </p>
  */
 package ru.d_shap.formmodel.binding;
