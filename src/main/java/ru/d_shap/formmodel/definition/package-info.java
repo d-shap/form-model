ee65fde1804c1e7c92d656aaea1ffb96c73a25b0
dmitry@DMITRY-DELL.Dlink <dmitry@DMITRY-DELL.Dlink>
2018-02-26 07:06:48 +0300

    Поправил javadoc

--- src/main/java/ru/d_shap/formmodel/definition/package-info.java
+++ src/main/java/ru/d_shap/formmodel/definition/package-info.java
@@ -18,6 +18,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
+ * <p>
  * Form definition elements.
+ * </p>
  */
 package ru.d_shap.formmodel.definition;
