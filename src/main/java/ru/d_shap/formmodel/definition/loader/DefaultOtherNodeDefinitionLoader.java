daf01b97bf83e54001b9c8c1d2ca8676b3f57716
Dmitry Shapovalov <shenf@mail.ru>
2018-08-30 15:19:49 +0300

    Класс по умолчанию для создания дополнительных элементов

---
+++ src/main/java/ru/d_shap/formmodel/definition/loader/DefaultOtherNodeDefinitionLoader.java
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.definition.loader;
+
+import org.w3c.dom.Element;
+
+import ru.d_shap.formmodel.definition.model.DefaultOtherNodeDefinition;
+import ru.d_shap.formmodel.definition.model.OtherNodeDefinition;
+
+/**
+ * Default loader for the other node definition.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class DefaultOtherNodeDefinitionLoader implements OtherNodeDefinitionLoader {
+
+    /**
+     * Create new object.
+     */
+    public DefaultOtherNodeDefinitionLoader() {
+        super();
+    }
+
+    @Override
+    public OtherNodeDefinition createOtherNodeDefinition(final Element element) {
+        return new DefaultOtherNodeDefinition(element);
+    }
+
+}
