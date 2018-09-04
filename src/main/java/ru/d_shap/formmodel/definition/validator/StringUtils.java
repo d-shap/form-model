49323392e4119011791bf85de43b6f7d136a28ba
Dmitry Shapovalov <shenf@mail.ru>
2018-09-04 15:04:33 +0300

    Вспомогательный класс для работы со строками

---
+++ src/main/java/ru/d_shap/formmodel/definition/validator/StringUtils.java
@@ -0,0 +1,95 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.definition.validator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Helper class to check string values.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class StringUtils {
+
+    private static final Set<Character> VALID_START_CHARACTERS;
+
+    private static final Set<Character> VALID_CHARACTERS;
+
+    static {
+        VALID_START_CHARACTERS = new HashSet<>();
+        for (char ch = 'a'; ch < 'z' + 1; ch++) {
+            VALID_START_CHARACTERS.add(ch);
+        }
+        for (char ch = 'A'; ch < 'Z' + 1; ch++) {
+            VALID_START_CHARACTERS.add(ch);
+        }
+        VALID_START_CHARACTERS.add('_');
+
+        VALID_CHARACTERS = new HashSet<>(VALID_START_CHARACTERS);
+        for (char ch = '0'; ch < '9' + 1; ch++) {
+            VALID_CHARACTERS.add(ch);
+        }
+        VALID_CHARACTERS.add('-');
+    }
+
+    private StringUtils() {
+        super();
+    }
+
+    /**
+     * Check if the string is null or empty.
+     *
+     * @param str the string to check.
+     *
+     * @return true if the string is null or empty.
+     */
+    public static boolean isEmptyString(final String str) {
+        if (str == null) {
+            return true;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if the string contains valid characters.
+     *
+     * @param str the string to check.
+     *
+     * @return true if the string contains valid characters.
+     */
+    public static boolean isValidCharactersInString(final String str) {
+        if (!VALID_START_CHARACTERS.contains(str.charAt(0))) {
+            return false;
+        }
+        for (int i = 1; i < str.length(); i++) {
+            if (!VALID_CHARACTERS.contains(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
