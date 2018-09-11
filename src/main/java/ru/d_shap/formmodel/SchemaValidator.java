960865fe20c0c9c7940e28fb62a006a25d7e76ff
Dmitry Shapovalov <shenf@mail.ru>
2018-09-11 23:22:31 +0300

    Выделил валидатор документа по схеме в отдельный класс

---
+++ src/main/java/ru/d_shap/formmodel/SchemaValidator.java
@@ -0,0 +1,115 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+/**
+ * XML document validator.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class SchemaValidator {
+
+    private static final DocumentBuilderFactory DOCUMENT_BUILDER_FACTORY;
+
+    static {
+        DOCUMENT_BUILDER_FACTORY = DocumentBuilderFactory.newInstance();
+        DOCUMENT_BUILDER_FACTORY.setNamespaceAware(true);
+    }
+
+    private static final SchemaFactory SCHEMA_FACTORY;
+
+    static {
+        SCHEMA_FACTORY = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    }
+
+    private static final String SCHEMA_PARENT_FOLDER = SchemaValidator.class.getPackage().getName().replaceAll("\\.", "/");
+
+    private static final String FORM_MODEL_SCHEMA_PATH = SCHEMA_PARENT_FOLDER + "/form-model-1_0.xsd";
+
+    private static final String FORM_INSTANCE_SCHEMA_PATH = SCHEMA_PARENT_FOLDER + "/form-instance-1_0.xsd";
+
+    private static final SchemaValidator FORM_MODEL_SCHEMA_VALIDATOR = new SchemaValidator(FORM_MODEL_SCHEMA_PATH);
+
+    private static final SchemaValidator FORM_INSTANCE_SCHEMA_VALIDATOR = new SchemaValidator(FORM_INSTANCE_SCHEMA_PATH);
+
+    private final Validator _validator;
+
+    private SchemaValidator(final String schemaPath) {
+        super();
+        URL url = getClass().getClassLoader().getResource(schemaPath);
+        try {
+            try (InputStream inputStream = url.openStream()) {
+                Source source = new StreamSource(inputStream);
+                Schema schema = SCHEMA_FACTORY.newSchema(source);
+                _validator = schema.newValidator();
+            }
+        } catch (IOException | SAXException ex) {
+            throw new SchemaValidatorException(ex);
+        }
+    }
+
+    /**
+     * Get the form-model schema validator.
+     *
+     * @return the form-model schema validator.
+     */
+    public static SchemaValidator getFormModelSchemaValidator() {
+        return FORM_MODEL_SCHEMA_VALIDATOR;
+    }
+
+    /**
+     * Get the form-instance schema validator.
+     *
+     * @return the form-instance schema validator.
+     */
+    public static SchemaValidator getFormInstanceSchemaValidator() {
+        return FORM_INSTANCE_SCHEMA_VALIDATOR;
+    }
+
+    /**
+     * Validate the XML document against the schema.
+     *
+     * @param document the XML document to validate.
+     */
+    public void validate(final Document document) {
+        try {
+            _validator.validate(new DOMSource(document));
+        } catch (IOException | SAXException ex) {
+            throw new SchemaValidatorException(ex);
+        }
+    }
+
+}
