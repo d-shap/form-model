0c165975cf6e9752a5e89fc76b8f29c4fbcef535
Dmitry Shapovalov <shenf@mail.ru>
2018-09-17 15:28:01 +0300

    Убрал константы

--- src/main/java/ru/d_shap/formmodel/binding/model/BindedAttribute.java
+++ src/main/java/ru/d_shap/formmodel/binding/model/BindedAttribute.java
@@ -26,6 +26,4 @@ package ru.d_shap.formmodel.binding.model;
  */
 public interface BindedAttribute {
 
-    String USER_DATA_KEY = BindedAttribute.class.getName() + "__USER_DATA_KEY__";
-
 }
--- src/main/java/ru/d_shap/formmodel/binding/model/BindedElement.java
+++ src/main/java/ru/d_shap/formmodel/binding/model/BindedElement.java
@@ -26,6 +26,4 @@ package ru.d_shap.formmodel.binding.model;
  */
 public interface BindedElement {
 
-    String USER_DATA_KEY = BindedElement.class.getName() + "__USER_DATA_KEY__";
-
 }
