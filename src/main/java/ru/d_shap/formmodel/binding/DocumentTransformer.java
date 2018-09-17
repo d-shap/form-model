1c2b3c33f0f530687ee3d4c0f16f9634b088705c
Dmitry Shapovalov <shenf@mail.ru>
2018-09-17 15:45:23 +0300

    Добавил вспомогательный класс для преобразования документа

---
+++ src/main/java/ru/d_shap/formmodel/binding/DocumentTransformer.java
@@ -0,0 +1,65 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+
+/**
+ * Document transformation helper class.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class DocumentTransformer {
+
+    private DocumentTransformer() {
+        super();
+    }
+
+    /**
+     * Write the XML document to the specified writer.
+     *
+     * @param document the XML document.
+     * @param writer   the specified writer
+     *
+     * @throws IOException          IO Exception.
+     * @throws TransformerException transformer exception.
+     */
+    public static void writeTo(final Document document, final Writer writer) throws IOException, TransformerException {
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.transform(new DOMSource(document), new StreamResult(writer));
+        } finally {
+            writer.close();
+        }
+    }
+
+}
