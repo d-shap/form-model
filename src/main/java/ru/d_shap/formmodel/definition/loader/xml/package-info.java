f08066d15e252cf26d4de95529a35ef7c0b37a6d
Dmitry Shapovalov <shenf@mail.ru>
2018-09-18 16:05:40 +0300

    Поправил javadoc

--- src/main/java/ru/d_shap/formmodel/definition/loader/xml/package-info.java
+++ src/main/java/ru/d_shap/formmodel/definition/loader/xml/package-info.java
@@ -18,6 +18,6 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * Form-model definition xml loader.
+ * Form-model definition loader, XML implementation.
  */
 package ru.d_shap.formmodel.definition.loader.xml;
