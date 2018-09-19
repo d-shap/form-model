a5d7522f159220b0dccb533f1bdf78ac8c2a2188
Dmitry Shapovalov <shenf@mail.ru>
2018-09-19 11:36:37 +0300

    Сделал final

--- src/test/java/ru/d_shap/formmodel/InputSourceReadExceptionTest.java
+++ src/test/java/ru/d_shap/formmodel/InputSourceReadExceptionTest.java
@@ -30,7 +30,7 @@ import ru.d_shap.assertions.Assertions;
  *
  * @author Dmitry Shapovalov
  */
-public class InputSourceReadExceptionTest {
+public final class InputSourceReadExceptionTest {
 
     /**
      * Test class constructor.
