8198166ca72a4e841ff3c9241273ddd747085ad0
Dmitry Shapovalov <shenf@mail.ru>
2018-09-20 16:01:40 +0300

    Тесты для ошибок определения модели

---
+++ src/test/java/ru/d_shap/formmodel/definition/FormDefinitionNotFoundExceptionTest.java
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.definition;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+
+/**
+ * Tests for {@link FormDefinitionNotFoundException}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class FormDefinitionNotFoundExceptionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public FormDefinitionNotFoundExceptionTest() {
+        super();
+    }
+
+    /**
+     * {@link FormDefinitionNotFoundException} class test.
+     */
+    @Test
+    public void errorMessageTest() {
+        Assertions.assertThat(new FormDefinitionNotFoundException(null, "id")).hasMessage("[Form definition was not found: @:id]");
+        Assertions.assertThat(new FormDefinitionNotFoundException("", "id")).hasMessage("[Form definition was not found: @:id]");
+        Assertions.assertThat(new FormDefinitionNotFoundException(" ", "id")).hasMessage("[Form definition was not found: @ :id]");
+        Assertions.assertThat(new FormDefinitionNotFoundException("group", null)).hasMessage("[Form definition was not found: @group:]");
+        Assertions.assertThat(new FormDefinitionNotFoundException("group", "")).hasMessage("[Form definition was not found: @group:]");
+        Assertions.assertThat(new FormDefinitionNotFoundException("group", " ")).hasMessage("[Form definition was not found: @group: ]");
+        Assertions.assertThat(new FormDefinitionNotFoundException("group", "id")).hasMessage("[Form definition was not found: @group:id]");
+    }
+
+}
