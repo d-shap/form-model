e24590c36fd225bb6e4c625295eb3fe1bc07bcde
Dmitry Shapovalov <shenf@mail.ru>
2018-09-26 12:39:29 +0300

    Вычисление группы вынес в утилитный класс

--- src/main/java/ru/d_shap/formmodel/definition/model/FormDefinition.java
+++ src/main/java/ru/d_shap/formmodel/definition/model/FormDefinition.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import java.util.Set;
 
 import ru.d_shap.formmodel.Messages;
+import ru.d_shap.formmodel.NullValueHelper;
 
 /**
  * Node definition to define the form.
@@ -78,11 +79,7 @@ public final class FormDefinition implements NodeDefinition {
      */
     public FormDefinition(final String group, final String id, final List<NodeDefinition> nodeDefinitions, final Map<String, String> otherAttributes, final String source) {
         super();
-        if (group == null) {
-            _group = "";
-        } else {
-            _group = group;
-        }
+        _group = NullValueHelper.getValue(group);
         _id = id;
         _nodeData = new NodeData(nodeDefinitions, CHILD_ELEMENT_NAMES, otherAttributes, ATTRIBUTE_NAMES);
         _source = source;
