882ded0c386ecd497e2a699cf0454d9188f22bee
Dmitry Shapovalov <shenf@mail.ru>
2018-09-28 15:24:43 +0300

    Тесты на вспомогательный класс обработки пустых значений

---
+++ src/test/java/ru/d_shap/formmodel/NullValueHelperTest.java
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+
+/**
+ * Tests for {@link NullValueHelper}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class NullValueHelperTest {
+
+    /**
+     * Test class constructor.
+     */
+    public NullValueHelperTest() {
+        super();
+    }
+
+    /**
+     * {@link Messages} class test.
+     */
+    @Test
+    public void constructorTest() {
+        Assertions.assertThat(NullValueHelper.class).hasOnePrivateConstructor();
+    }
+
+    /**
+     * {@link NullValueHelper} class test.
+     */
+    @Test
+    public void getValueTest() {
+        Assertions.assertThat(NullValueHelper.getValue(null)).isEqualTo("");
+        Assertions.assertThat(NullValueHelper.getValue("")).isEqualTo("");
+        Assertions.assertThat(NullValueHelper.getValue(" ")).isEqualTo(" ");
+        Assertions.assertThat(NullValueHelper.getValue("value")).isEqualTo("value");
+    }
+
+}
