412602cec69f1dbee96393766ac30e849fe0b7b2
Dmitry Shapovalov <shenf@mail.ru>
2018-10-08 17:23:32 +0300

    Добавил заглушки для тестов связывания

---
+++ src/test/java/ru/d_shap/formmodel/binding/FormInstanceBuilderImplTest.java
@@ -0,0 +1,140 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model library is a form definition API and a form binding API.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model library.
+//
+// Form model library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.formmodel.BaseFormModelTest;
+import ru.d_shap.formmodel.ServiceFinder;
+import ru.d_shap.formmodel.XmlDocumentBuilder;
+import ru.d_shap.formmodel.binding.model.BindingSourceImpl;
+import ru.d_shap.formmodel.definition.loader.xml.FormXmlDefinitionsElementLoader;
+import ru.d_shap.formmodel.definition.model.FormDefinition;
+import ru.d_shap.formmodel.definition.model.FormDefinitions;
+
+/**
+ * Tests for {@link FormInstanceBuilderImpl}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class FormInstanceBuilderImplTest extends BaseFormModelTest {
+
+    /**
+     * Test class constructor.
+     */
+    public FormInstanceBuilderImplTest() {
+        super();
+    }
+
+    /**
+     * {@link FormInstanceBuilderImpl} class test.
+     */
+    @Test
+    public void buildFormInstanceTest() {
+        String xml1 = "<?xml version='1.0'?>\n";
+        xml1 += "<ns1:form id='id1' xmlns:ns1='http://d-shap.ru/schema/form-model/1.0'>";
+        xml1 += "</ns1:form>";
+        FormDefinitions formDefinitions1 = createFormDefinitions(xml1);
+        FormInstanceBuilderImpl formInstanceBuilder1 = createBinder(formDefinitions1);
+        Document document1 = newDocument();
+        formInstanceBuilder1.buildFormInstance(new BindingSourceImpl("repr1"), document1, formDefinitions1.getFormDefinition("id1"));
+
+        try {
+            String xml2 = "<?xml version='1.0'?>\n";
+            xml2 += "<ns1:form id='id2' xmlns:ns1='http://d-shap.ru/schema/form-model/1.0'>";
+            xml2 += "</ns1:form>";
+            FormDefinitions formDefinitions2 = createFormDefinitions(xml2);
+            FormInstanceBuilderImpl formInstanceBuilder2 = createBinder(formDefinitions2);
+            Document document2 = newDocument();
+            formInstanceBuilder2.buildFormInstance(new BindingSourceImpl(null), document2, formDefinitions2.getFormDefinition("id2"));
+            Assertions.fail("FormInstanceBuilderImpl test fail");
+        } catch (FormBindingException ex) {
+            Assertions.assertThat(ex).hasMessage("[Form is not present: {source}form[@:id2]]");
+        }
+    }
+
+    /**
+     * {@link FormInstanceBuilderImpl} class test.
+     */
+    @Test
+    public void buildAttributeInstanceTest() {
+
+    }
+
+    /**
+     * {@link FormInstanceBuilderImpl} class test.
+     */
+    @Test
+    public void buildElementInstanceTest() {
+
+    }
+
+    /**
+     * {@link FormInstanceBuilderImpl} class test.
+     */
+    @Test
+    public void buildSingleElementInstanceTest() {
+
+    }
+
+    /**
+     * {@link FormInstanceBuilderImpl} class test.
+     */
+    @Test
+    public void buildFormReferenceInstanceTest() {
+
+    }
+
+    /**
+     * {@link FormInstanceBuilderImpl} class test.
+     */
+    @Test
+    public void buildOtherNodeInstanceTest() {
+
+    }
+
+    private FormDefinitions createFormDefinitions(final String... xmls) {
+        List<FormDefinition> formDefinitions1 = new ArrayList<>();
+        for (String xml : xmls) {
+            Document document = parse(xml);
+            FormXmlDefinitionsElementLoader formXmlDefinitionsElementLoader = new FormXmlDefinitionsElementLoader(document.getDocumentElement(), "source");
+            List<FormDefinition> formDefinitions2 = formXmlDefinitionsElementLoader.load();
+            formDefinitions1.addAll(formDefinitions2);
+        }
+        FormDefinitions formDefinitions = new FormDefinitions();
+        formDefinitions.addFormDefinitions(formDefinitions1);
+        return formDefinitions;
+    }
+
+    private FormInstanceBuilderImpl createBinder(final FormDefinitions formDefinitions) {
+        List<OtherNodeInstanceBuilder> otherNodeInstanceBuilders = ServiceFinder.find(OtherNodeInstanceBuilder.class);
+        return new FormInstanceBuilderImpl(formDefinitions, new FormInstanceBinderImpl(), otherNodeInstanceBuilders);
+    }
+
+    private Document newDocument() {
+        return XmlDocumentBuilder.getDocumentBuilder().newDocument();
+    }
+
+}
